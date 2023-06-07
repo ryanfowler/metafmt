da75ae40e5a8516de4b7f8b7f5185fc6e5f4f80b
Ryan Fowler <ryan.fowler19@gmail.com>
2023-06-07 10:12:14 -0700

    Rename Go yaml directory

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     cgo::Build::new()
         .trimpath(true)
         .ldflags("-s -w")
-        .package("pkg/yaml.go")
+        .package("metayaml/main.go")
         .build("metayaml");
 
     println!("cargo:rerun-if-changed=pkg");

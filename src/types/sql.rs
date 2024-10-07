679c218e1e5e4d177791749cd33b8ff9cfd8ccf4
Ryan Fowler <ryan.fowler19@gmail.com>
2024-10-07 06:41:26 -0700

    Fix sql formatting for sqlformat v0.3.0

--- src/types/sql.rs
+++ src/types/sql.rs
@@ -8,10 +8,10 @@ pub(crate) struct Sql {}
 impl Format for Sql {
     fn format(&self, input: &str) -> Result<String, String> {
         let opts = FormatOptions {
-            uppercase: true,
+            uppercase: Some(true),
             ..FormatOptions::default()
         };
-        let mut out = format(input, &QueryParams::None, opts);
+        let mut out = format(input, &QueryParams::None, &opts);
         out.push('\n');
         Ok(out)
     }
